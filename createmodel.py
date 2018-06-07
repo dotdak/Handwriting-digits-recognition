4cb4a70c328d379de710a8aebb6929a20b5b9be8
dotdak <honghung.dotdak@gmail.com>
2018-06-07 08:43:44 +0700

    Update createmodel.py

--- createmodel.py
+++ createmodel.py
@@ -67,7 +67,7 @@ y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # Define loss and optimizer
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
